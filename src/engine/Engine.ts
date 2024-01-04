878322b6ba8a4f56d07c30259969c1a7760f2cc0
Omar <omarfkuri@gmail.com>
2024-01-03 23:14:29 -0600

    Check frames

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -4,14 +4,43 @@ export abstract class Engine
   {}
 
   #running = true;
+  #lastTime = 0;
+  #timer = 0;
+  #deltaTime = 0;
+  #nowTime = 0;
+
+  protected frames = 0;
+  protected updates = 0;
+  protected maxUpdates = 60;
 
   public async run()
   {
+    this.#lastTime = Date.now();
+    this.#timer = this.#lastTime;
+
     await this.init();
     while(this.#running)
     {
       await this.render();
       await this.update();
+
+      this.#nowTime = Date.now();
+      this.#deltaTime += (this.#nowTime - this.#lastTime) / this.maxUpdates;
+      this.#lastTime = this.#nowTime;
+      
+      while (this.#deltaTime >= 1000){
+        this.update();
+        this.updates++;
+        this.#deltaTime--;
+      }
+
+      this.frames++;
+
+      if (Date.now() - this.#timer > 1000) {
+        this.#timer ++;
+        this.updates = 0; 
+        this.frames = 0;
+      }
 
       const error = GL.getError();
       if (error !== GL.NO_ERROR) {
